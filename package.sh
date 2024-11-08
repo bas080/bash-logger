41f7b98a6a620cf3d9bb60f0860febb836276eac
bas081 <bas080@hotmail.com>
2024-11-08 17:07:54 +0200

    Mock the date command

--- package.sh
+++ package.sh
@@ -7,6 +7,15 @@ linked cmp
 linked xargs
 linked tee
 linked printf
+linked date "$PWD/date"
+linked spat
+linked diff
+linked cat
+linked rm
+linked touch
+linked markatzea
+linked perl
+linked grep
 
 # dev
 linked bash-logger "$PWD/bash-logger"
