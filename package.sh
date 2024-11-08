7824b468cf4169c1a0a57c0fefd58e93d78bfb8f
bas081 <bas080@hotmail.com>
2024-11-08 17:10:14 +0200

    fixup! Mock the date command

--- package.sh
+++ package.sh
@@ -1,7 +1,5 @@
 version 0.0.0
 
-sourceable bash-snapshot https://raw.githubusercontent.com/bas080/bash-snapshot/master/bash-snapshot
-sourceable bash-tap      https://raw.githubusercontent.com/bas080/bash-tap/master/bash-tap
 linked bash
 linked cmp
 linked xargs
@@ -16,6 +14,4 @@ linked touch
 linked markatzea
 linked perl
 linked grep
-
-# dev
 linked bash-logger "$PWD/bash-logger"
